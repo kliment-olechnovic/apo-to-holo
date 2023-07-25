18c76767fb863b9e01fe72b78d765e8ef430abb3
kliment-olechnovic <kliment.olechnovic@gmail.com>
2023-07-25 14:23:44 +0200

    Improved assessment workflow script.

--- processes/assess_coarse_grained_predictions/workflow.bash
+++ processes/assess_coarse_grained_predictions/workflow.bash
@@ -2,15 +2,17 @@
 
 cd "$(dirname $0)"
 
-for SETNAME in validation training
+find ./input/ -mindepth 3 -maxdepth 3 -type d | sort -V \
+| while read INDIR
 do
-	find ./input/${SETNAME}/ -mindepth 1 -maxdepth 1 -type d | sort -V \
-	| while read INDIR
-	do
-		OUTDIR="$(echo ${INDIR} | sed 's|/input/|/output/|')"
-		./assess_predictions_in_directory.bash "$OUTDIR" "$INDIR"
-	done
-	
-	montage $(find ./output/${SETNAME}/ -type f -name 'all_pairs.png' | sort -V) -geometry +0+0 -tile 3x ./output/plots_of_${SETNAME}_pairs.png
+	OUTDIR="$(echo ${INDIR} | sed 's|/input/|/output/|')"
+	./assess_predictions_in_directory.bash "$OUTDIR" "$INDIR"
+done
+
+find ./output/ -mindepth 2 -maxdepth 2 -type d | sort -V \
+| while read OUTDIR
+do
+	SETNAME="$(basename ${OUTDIR})"
+	montage $(find ${OUTDIR}/ -type f -name 'all_pairs.png' | sort -V) -geometry +0+0 -tile 3x ${OUTDIR}/../plots_of_pairs_from_${SETNAME}.png
 done
 
