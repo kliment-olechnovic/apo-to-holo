d7cd79bd71e3529828672a3c9e37a7dcb21e6c72
kliment-olechnovic <kliment.olechnovic@gmail.com>
2023-07-25 12:58:48 +0200

    Improved assessment workflow script.

--- processes/assess_coarse_grained_predictions/workflow.bash
+++ processes/assess_coarse_grained_predictions/workflow.bash
@@ -2,13 +2,15 @@
 
 cd "$(dirname $0)"
 
-for EPOCHNUM in 20 40 60 80 100 120 200
-do
-	./assess_predictions_in_directory.bash ./output/validation/epoch${EPOCHNUM} ./input/validation/epoch${EPOCHNUM}.pth
-	./assess_predictions_in_directory.bash ./output/training/epoch${EPOCHNUM} ./input/training/epoch${EPOCHNUM}.pth
-done
-
 for SETNAME in validation training
 do
+	find ./input/${SETNAME}/ -mindepth 1 -maxdepth 1 -type d | sort -V \
+	| while read INDIR
+	do
+		OUTDIR="$(echo ${INDIR} | sed 's|/input/|/output/|')"
+		./assess_predictions_in_directory.bash "$OUTDIR" "$INDIR"
+	done
+	
 	montage $(find ./output/${SETNAME}/ -type f -name 'all_pairs.png' | sort -V) -geometry +0+0 -tile 3x ./output/plots_of_${SETNAME}_pairs.png
 done
+
